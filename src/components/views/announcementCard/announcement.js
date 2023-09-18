f4d3b96e6ade5432088ad3d04934aca60d9904f3
dani24sahu <daniramsahu2001@gmail.com>
2023-09-18 18:00:34 +0530

    separated announcement data from announcement component

---
+++ src/components/views/announcementCard/announcement.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    subject: "Regarding New feature release",
+    content:
+      "We are excited to announce that we have launched a new feature that will help you to manage your tasks more efficiently. You can now create subtasks, assign them to different members, and track their progress. To learn more about this feature, please visit our documentation page [here](https://ant.design/components/card/).",
+    date: "2023-09-23",
+    author: {
+      name: "Alice",
+      role: "HR Manager",
+      avatar: "https://source.unsplash.com/random",
+    },
+  },
+  {
+    id: 2,
+    subject: "Regarding Scheduled maintenance",
+    content:
+      "We will be performing a scheduled maintenance on our servers on Sunday, September 27, from 10:00 AM to 12:00 PM (UTC). During this time, you may experience some interruptions in service. We apologize for any inconvenience this may cause and thank you for your patience and understanding.",
+    date: "2023-09-14",
+    author: {
+      name: "Bob",
+      role: "HR Manager",
+      avatar: "https://source.unsplash.com/random",
+    },
+  },
+];
