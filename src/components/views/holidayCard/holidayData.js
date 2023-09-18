2dc37158eac86dd825fe15d527bf3e9fcc115c81
dani24sahu <daniramsahu2001@gmail.com>
2023-09-18 15:14:12 +0530

    created separate file for holiday data

---
+++ src/components/views/holidayCard/holidayData.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    date: "01 Jan",
+    occasion: "New Year",
+    type: "National",
+  },
+  {
+    date: "26 Jan",
+    occasion: "Republic Day",
+    type: "National",
+  },
+  {
+    date: "29 Mar",
+    occasion: "Holi",
+    type: "Public",
+  },
+  {
+    date: "15 Aug",
+    occasion: "Independence Day",
+    type: "National",
+  },
+  {
+    date: "02 Oct",
+    occasion: "Gandhi Jayanti",
+    type: "National",
+  },
+];
