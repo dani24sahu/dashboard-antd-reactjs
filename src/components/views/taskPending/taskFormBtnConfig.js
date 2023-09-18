f37354b111cb939da4ab6cafeb24ebcc9c1515fd
dani24sahu <daniramsahu2001@gmail.com>
2023-09-18 12:49:49 +0530

    removed unnecessary comments

--- src/components/views/taskPending/taskFormBtnConfig.js
+++ src/components/views/taskPending/taskFormBtnConfig.js
@@ -1,38 +1,38 @@
 export const buttonConfig = [
-    {
-      id: 1,
-      label: "Apply New",
-      type: "primary",
-      style: { color: "white", backgroundColor: "maroon" }, // Custom style
-    },
-    {
-      id: 2,
-      label: "Submit",
-      type: "primary",
-      style: { color: "white", backgroundColor: "blue" }, // Custom style
-    },
-    {
-      id: 3,
-      label: "Cancel",
-      type: "ghost",
-      style: { color: "black", backgroundColor: "silver" }, // Custom style
-    },
-    {
-      id: 4,
-      label: "Add New Category",
-      type: "primary",
-      style: { color: "white", backgroundColor: "blue" }, // Custom style
-    },
-    {
-      id: 5,
-      label: "Delete",
-      type: "primary",
-      style: { color: "white", backgroundColor: "red" }, // Custom style
-    },
-    {
-      id: 6,
-      label: "New Category",
-      type: "primary",
-      style: { color: "white", backgroundColor: "blue" },
-    },
-  ];
+  {
+    id: 1,
+    label: "Apply New",
+    type: "primary",
+    style: { color: "white", backgroundColor: "maroon" },
+  },
+  {
+    id: 2,
+    label: "Submit",
+    type: "primary",
+    style: { color: "white", backgroundColor: "blue" },
+  },
+  {
+    id: 3,
+    label: "Cancel",
+    type: "ghost",
+    style: { color: "black", backgroundColor: "silver" },
+  },
+  {
+    id: 4,
+    label: "Add New Category",
+    type: "primary",
+    style: { color: "white", backgroundColor: "blue" },
+  },
+  {
+    id: 5,
+    label: "Delete",
+    type: "primary",
+    style: { color: "white", backgroundColor: "red" },
+  },
+  {
+    id: 6,
+    label: "New Category",
+    type: "primary",
+    style: { color: "white", backgroundColor: "blue" },
+  },
+];
