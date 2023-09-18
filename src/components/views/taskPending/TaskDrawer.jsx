2f9f01f533dbd37baa8ea82b58bcf70ec6b6ce12
dani24sahu <daniramsahu2001@gmail.com>
2023-09-18 12:42:07 +0530

    Created a Drawer to view full task lists

--- src/components/views/taskPending/TaskDrawer.jsx
+++ src/components/views/taskPending/TaskDrawer.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { Drawer } from "antd";
-const App = () => {
+const App = ({title, placement, closable, onClose, open, width}) => {
   return (
     <>
       <Drawer
-        title="Basic Drawer"
+        title={title}
         placement={placement}
         closable={false}
         onClose={onClose}
         open={open}
         key={placement}
+        width={width}
       >
         <p>Some contents...</p>
         <p>Some contents...</p>
