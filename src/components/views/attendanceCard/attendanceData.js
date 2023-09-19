fd2716e1e40e82d7b9fc884585df2d671b2444b3
dani24sahu <daniramsahu2001@gmail.com>
2023-09-19 15:29:11 +0530

    created separate attendance data file

---
+++ src/components/views/attendanceCard/attendanceData.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    key: "1",
+    type: "Present",
+    count: 20,
+  },
+  {
+    key: "2",
+    type: "Absent",
+    count: 0,
+  },
+  {
+    key: "3",
+    type: "Holiday",
+    count: 0,
+  },
+  {
+    key: "4",
+    type: "Leaves",
+    count: 0,
+  },
+  {
+    key: "5",
+    type: "Weekend",
+    count: 4,
+  },
+  {
+    key: "6",
+    type: "Penalty",
+    count: 0,
+  },
+];
