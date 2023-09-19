da96b1a08a0984d2f6804dc74c5e897829a9893f
dani24sahu <daniramsahu2001@gmail.com>
2023-09-19 15:42:07 +0530

    fixed card size and added scroll for overflow

--- src/components/views/pendingOnMe/Pending.jsx
+++ src/components/views/pendingOnMe/Pending.jsx
@@ -1,14 +1,14 @@
 import { Card, List } from "antd";
 import React from "react";
 
-const Pending = ({data}) => {
+const Pending = () => {
   return (
     <>
       <Card title="Pending on me" bordered={false}>
         <List
           size="small"
           bordered={false}
-          dataSource={data}
+          style={{ overflow: "auto", height: 300 }}
           renderItem={(item) => <List.Item>{item}</List.Item>}
         />
       </Card>
