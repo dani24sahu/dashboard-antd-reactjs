674cd1824e7fd6554a5c0ceb824b36cd4513013e
dani24sahu <daniramsahu2001@gmail.com>
2023-09-19 17:24:05 +0530

    separated insights data

---
+++ src/components/views/insightCard/insightsData.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    key: "1",
+    name: "Alice Smith (1001)",
+    date: "19-Sep-2023",
+    type: ["Paid Leave"],
+    status: "approved",
+  },
+  {
+    key: "2",
+    name: "Bob Jones (1002)",
+    date: "20-Sep-2023",
+    type: ["Wedding Leave"],
+    status: "pending",
+  },
+  {
+    key: "3",
+    name: "Charlie Brown (1003)",
+    date: "21-Sep-2023",
+    type: ["Paternity Leave"],
+    status: "declined",
+  },
+  {
+    key: "4",
+    name: "David Lee (1004)",
+    date: "22-Sep-2023",
+    type: ["Work from home"],
+    status: "approved",
+  },
+  {
+    key: "5",
+    name: "Emma Watson (1005)",
+    date: "23-Sep-2023",
+    type: ["Work from home"],
+    status: "pending",
+  },
+  {
+    key: "6",
+    name: "Frank Miller (1006)",
+    date: "24-Sep-2023",
+    type: ["On duty"],
+    status: "approved",
+  },
+];
