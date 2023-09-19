cecfe1161a4980325ae3f94bd8c65ad49063c938
dani24sahu <daniramsahu2001@gmail.com>
2023-09-19 16:24:49 +0530

    added a drawer to view full leave data

--- src/components/views/leavesCard/Leaves.jsx
+++ src/components/views/leavesCard/Leaves.jsx
@@ -1,18 +1,104 @@
-import { Card, List } from "antd";
-import React from "react";
+import React, { useState } from "react";
+import { Card, Table, Drawer } from "antd";
+import { FullscreenOutlined } from "@ant-design/icons";
+import {data} from './leavesData'
 
-const Leaves = ({data}) => {
+const cardColumns = [
+  {
+    dataIndex: "type",
+    key: "type",
+  },
+  {
+    dataIndex: "value",
+    key: "value",
+    align: "right",
+  },
+];
+
+const drawerColumns = [
+  {
+    title: "Type",
+    dataIndex: "type",
+    key: "type",
+  },
+  {
+    title: "Added This Month",
+    dataIndex: "added",
+    key: "added",
+    align: "right",
+  },
+  {
+    title: "Current Balance",
+    dataIndex: "balance",
+    key: "balance",
+    align: "right",
+  },
+];
+
+const Leaves = () => {
+  const [visible, setVisible] = useState(false);
+  const [selectedRow, setSelectedRow] = useState(null);
+
+  const handleOpenDrawer = () => {
+    setVisible(true);
+  };
+
+  const handleCloseDrawer = () => {
+    setVisible(false);
+  };
+
+  const handleSelectRow = (record) => {
+    setSelectedRow(record);
+  };
+
+  const extraContent = <FullscreenOutlined onClick={handleOpenDrawer} />;
+
+  const drawerContent = (
+    <div>
+      <Table
+        columns={drawerColumns}
+        dataSource={data}
+        pagination={false}
+        onRow={(record) => ({
+          onClick: () => handleSelectRow(record),
+        })}
+      />
+      {selectedRow && (
+        <aside>
+          <h3>{selectedRow.type}</h3>
+          <Table
+            columns={[
+              { dataIndex: "key", key: "key" },
+              { dataIndex: "value", key: "value", align: "right" },
+            ]}
+            dataSource={[
+              { key: "Current Balance", value: selectedRow.balance },
+              { key: "Added This Month", value: selectedRow.added },
+              { key: "Leaves Taken", value: null },
+              { key: "Leaves Pending", value: null },
+              { key: "Total Remaining", value: null },
+              { key: "Carry Forward", value: null },
+            ]}
+            pagination={false}
+            showHeader={false}
+          />
+        </aside>
+      )}
+    </div>
+  );
+
   return (
-    <>
-      <Card title="Leaves" bordered={false}>
-        <List
-          size="small"
-          bordered={false}
-          dataSource={data}
-          renderItem={(item) => <List.Item>{item}</List.Item>}
-        />
-      </Card>
-    </>
+    <Card title="Leaves" extra={extraContent} style={{ width: 500 }}>
+      <Table columns={cardColumns} dataSource={data} pagination={false} />
+      <Drawer
+        title="Leaves"
+        placement="bottom"
+        open={visible}
+        onClose={handleCloseDrawer}
+      >
+        {drawerContent}
+      </Drawer>
+    </Card>
   );
 };
 
