1046a86d2d77660b4cf1ee26c328efaee50fd652
dani24sahu <daniramsahu2001@gmail.com>
2023-09-19 16:24:14 +0530

    separated leaves data

---
+++ src/components/views/leavesCard/leavesData.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    key: "1",
+    type: "Current Balance",
+    value: 16.0,
+    added: null,
+    balance: null,
+  },
+  {
+    key: "2",
+    type: "Paid Leave",
+    value: 6.0,
+    added: 0.0,
+    balance: 6.0,
+  },
+  {
+    key: "3",
+    type: "Paternity Leave",
+    value: 5.0,
+    added: 0.0,
+    balance: 5.0,
+  },
+  {
+    key: "4",
+    type: "Wedding Leaves",
+    value: 5.0,
+    added: 0.0,
+    balance: 5.0,
+  },
+  {
+    key: "5",
+    type: "LOP",
+    value: null,
+    added: null,
+    balance: null,
+  },
+];
