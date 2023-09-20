eeb5eb6fb4bd2867f1510b8edb8be1a3c7c9e898
dani24sahu <daniramsahu2001@gmail.com>
2023-09-20 18:26:25 +0530

    created left and right side icon buttons

---
+++ src/components/stories/Button/IconButton.stories.js
@@ -0,0 +1,37 @@
+import { DeleteOutlined, SearchOutlined } from "@ant-design/icons";
+import IconButton from "./IconButton";
+
+export default {
+  title: "Buttons/Icon Button",
+  component: IconButton,
+};
+
+export const LeftIcon = (args) => <IconButton {...args} />;
+
+LeftIcon.args = {
+  icon: <DeleteOutlined />,
+  iconPosition: 'left',
+  label: 'Delete',
+  onClick: () => alert('You clicked delete'),
+};
+
+LeftIcon.argTypes = {
+  icon: {
+    control: {
+      type: 'select',
+      options: [<DeleteOutlined />, <SearchOutlined />],
+    },
+  },
+  iconPosition: {
+    control: {
+      type: 'radio',
+      options: ['left', 'right'],
+    },
+  },
+};
+export const RightIcon = (args) => <IconButton {...args} />;
+
+RightIcon.args = {
+  ...LeftIcon.args,
+  iconPosition: 'right',
+};
