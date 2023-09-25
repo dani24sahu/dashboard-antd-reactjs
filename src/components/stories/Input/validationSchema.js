522bb2b42c710040fc5ec99f8856d83a433e46e9
dani24sahu <daniramsahu2001@gmail.com>
2023-09-25 19:02:30 +0530

    crated validation for URL input

--- src/components/stories/Input/validationSchema.js
+++ src/components/stories/Input/validationSchema.js
@@ -1,7 +1,6 @@
 import Joi from "joi";
 
-export const emailSchema = Joi.string()
-.custom((value, helpers) => {
+export const emailSchema = Joi.string().custom((value, helpers) => {
   if (/^.+@.+\..+$/.test(value)) {
     return value; // Valid email format
   }
@@ -13,3 +12,7 @@ export const ageSchema = Joi.number().integer().min(18).max(100).required();
 export const genderSchema = Joi.string()
   .valid("Male", "Female", "Other")
   .required();
+export const urlSchema = Joi.string()
+  .uri({ scheme: ["http", "https"] })
+  .required()
+  .error(new Error("Invalid URL format. Only HTTP/HTTPS links are allowed."));
